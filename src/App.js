63c8ce631512cfe74eebb5bb44af4910faa5250f
dnjswns619 <dnjswns619@naver.com>
2024-02-07 16:18:00 +0900

    add banner of components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import SwiperSlider from './components/SwiperSlider.js';
 import EventDetailLink from './components/EventDetailLink.js';
 import JustDropped from './components/JustDropped.js';
 import TopBrandLink from './components/TopBrand.js';
+import EventBanner from './components/EventBanner.js';
+import eventBannerData from './eventBannerData.js';
 
 function App() {
   return (
@@ -11,9 +13,13 @@ function App() {
       <Header></Header>
 
       <SwiperSlider></SwiperSlider>
+
       <EventDetailLink></EventDetailLink>
+      
       <JustDropped></JustDropped>
       <TopBrandLink></TopBrandLink>
+
+      <EventBanner item={eventBannerData[0]}></EventBanner>
     </div>
   );
 }
---
+++ src/components/EventBanner.js
@@ -0,0 +1,14 @@
+import "../css/EventBanner.css"
+function EventBanner(props) {
+  return (
+    <div className="bannerWrap" style={{backgroundColor : props.item.bgColor}}>
+      <picture>
+        <source srcSet={process.env.PUBLIC_URL + props.item.src} type='image/webp'>
+        </source>
+        <img src={process.env.PUBLIC_URL + props.item.src}></img>
+      </picture>
+    </div>
+  )
+}
+
+export default EventBanner;
