99ae0d059f38dec489ec71aadf0125535d2036a8
dnjswns619 <dnjswns619@naver.com>
2024-02-14 15:18:17 +0900

    create theNorthFace section

---
+++ src/components/NorthFace.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import SaleItem from "./SaleItem.js";
+import ShowMoreBtn from "./ShowMoreBtn.js";
+import northFaceData from "../northFaceData.js";
+
+function NorthFace() {
+  const [showItem, setShowItem] = useState(5);
+  let showDroppedItemCount = northFaceData.slice(0, showItem);
+  return (
+    <div className="container">
+      <div className="content-title">
+        <div className="title-wrap">
+          <h2>The North Face</h2>
+          <span className="subTitle">트랜드를 넘어 클래식으로</span>
+        </div>
+      </div>
+      <div className="content-item">
+        {
+          showDroppedItemCount.map((item, idx) => {
+            return <SaleItem item={item} key={item.id}></SaleItem>
+          })
+        }
+      </div>
+      <ShowMoreBtn showItem={showItem} setShowItem={setShowItem} data={northFaceData}></ShowMoreBtn>
+    </div>
+  )
+}
+
+export default NorthFace;
