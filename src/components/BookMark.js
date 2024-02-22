bf10e36086380329834316381a9021169e5a6ad4
dnjswns619 <dnjswns619@naver.com>
2024-02-22 14:23:14 +0900

    fix files

--- src/components/BookMark.js
+++ src/components/BookMark.js
@@ -5,7 +5,7 @@ function Bookmark(props) {
   const bookmarkCLickFunction = useCallback(() => {
     setBookmarkState(props.item.bookmarkState);
     props.item.bookmarkState = !props.item.bookmarkState;
-  })
+  }, [props.item])
   if(!props.item.bookmarkState) {
     return (
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark text-active" viewBox="0 0 16 16" onClick={bookmarkCLickFunction}>
--- src/components/SaleItem.js
+++ src/components/SaleItem.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Bookmark from "./BookMark.js";
 
 function SaleItem(props) {
